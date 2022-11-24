33f5eb3661877a814c4299a4ee6a0e2cdf913438
Adam Hoar <adam.hoar@icloud.com>
2022-11-24 15:06:21 +0000

    Created method to filter for names based on first character

--- directory.rb
+++ directory.rb
@@ -1,3 +1,18 @@
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -18,4 +33,17 @@ students
 # Returns array of students
 end
 
+# Asks for user for starting character they want to filter for
+def name_by_letter(students)
+  puts "Select the first letter you want to match with"
+  selected_students = []
+  letter = gets.chomp
+  selected_students << students.select {|student| student[:name].start_with?(letter)}
+  puts selected_students
+end
+
 students = input_students
+print_header
+print(students)
+print_footer(students)
+name_by_letter(students)
